8adbcc95e251d90e7164e2edf06519c05194776d
Alejandro Raffo <amsterdamAL@gmail.com>
2024-06-14 18:13:27 -0400

    header

--- src/components/companyLogo.tsx
+++ src/components/companyLogo.tsx
@@ -6,9 +6,9 @@ import tag_line from "@/images/tagline2.svg";
 
 export default function CompanyLogo() {
   return (
-    <div className="grid grid-cols-1 w-[200px] ml-[15px] justify-self-start self-center cl_bp_520:w-[265px]">
+    <div className="grid grid-cols-1 gap-[10px] w-[171px] ml-[15px] mb-[8px] justify-self-start self-center cl_bp_295:w-[200px] cl_bp_295:top-[4px] cl_bp_295:ml-[20px] cl_bp_520:w-[265px] cl_bp_731:w-[300px] cl_bp_985:ml-[50px] cl_bp_985:w-[300px] absolute top-4px z-51 cursor-pointer ease-linear duration-[.3s]">
       <Image
-        className="justify-self-start self-center w-250"
+        className="justify-self-start w-[95%] -ml-[2px] cl_bp_295:ml-[initial]"
         src={company_logo}
         alt="floridablaze home"
         width={300}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,8 @@ import leaf from "@/images/headerLeafv2.png";
 import CompanyLogo from "@/components/companyLogo";
 export default function Header() {
   return (
-    <div className="relative grid place-items-stretch grid-cols-[minmax(180px,280px)1fr] grid-rows-[69px] mt-[15px]">
+    <div className="relative grid grid-cols-[minmax(180px,280px)1fr] grid-rows-[69px] gap-[8px] mt-[15px] max-w-[2000px] overflow-hidden cl_bp_520:grid-rows-[80px]">
+      <div className="w-full" />
       <CompanyLogo />
       <Image
         className="hidden leaf_bp_520px:block w-full h-full max-w-[420px] min-w-[250px] justify-self-end self-center"
