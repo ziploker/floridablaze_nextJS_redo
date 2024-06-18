5f44841d1a0a23842f2935aca27058bfb5f06d79
Alejandro Raffo <amsterdamAL@gmail.com>
2024-06-18 16:29:16 -0400

    added topBar to header

--- src/components/companyLogo.tsx
+++ src/components/companyLogo.tsx
@@ -1,63 +1,63 @@
-"use client";
-import Image from "next/image";
-import React, { useEffect, useState, useRef } from "react";
+"use client"
+import Image from "next/image"
+import React, { useEffect, useState, useRef } from "react"
 
-import company_logo from "@/images/company_logo.svg";
-import tag_line from "@/images/tagline.svg";
+import company_logo from "@/images/company_logo.svg"
+import tag_line from "@/images/tagline.svg"
 
 interface Props {
-  logoScrolled: string;
-  setLogoScrolled: (word: string) => void;
+	logoScrolled: string
+	setLogoScrolled: (word: string) => void
 }
 
 export default function CompanyLogo(props: Props) {
-  const [pixlesFromLogoToTop, setPixlesFromLogoToTop] = useState(1);
-  const logoTextRef = useRef<HTMLInputElement>(null);
+	const [pixlesFromLogoToTop, setPixlesFromLogoToTop] = useState(1)
+	const logoTextRef = useRef<HTMLInputElement>(null)
 
-  useEffect(() => {
-    if (logoTextRef.current) {
-      setPixlesFromLogoToTop(
-        logoTextRef.current.getBoundingClientRect().top //-30
-      );
-    }
+	useEffect(() => {
+		if (logoTextRef.current) {
+			setPixlesFromLogoToTop(
+				logoTextRef.current.getBoundingClientRect().top //-30
+			)
+		}
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+		window.addEventListener("scroll", handleScroll)
+		return () => window.removeEventListener("scroll", handleScroll)
+	}, [])
 
-  const handleScroll = () => {
-    // window.scrollY >= pixlesFromLogoToTop - 4
-    window.scrollY >= pixlesFromLogoToTop + 1
-      ? props.setLogoScrolled("true")
-      : props.setLogoScrolled("false");
-  };
+	const handleScroll = () => {
+		// window.scrollY >= pixlesFromLogoToTop - 4
+		window.scrollY >= pixlesFromLogoToTop + 1
+			? props.setLogoScrolled("true")
+			: props.setLogoScrolled("false")
+	}
 
-  return (
-    <div
-      ref={logoTextRef}
-      className={`grid grid-cols-1 gap-[10px] w-[171px] ml-[15px] mb-[8px] justify-self-start self-center cl_bp_295:ml-[20px]  ${
-        props.logoScrolled === "true"
-          ? "fixed top-[14px] cl_bp_731:w-[260px] cl_bp_520:top-[10px] cl_bp_295:w-[160px] cl_bp_520:w-[225px] cl_bp_985:w-[260px]"
-          : "absolute top-[4px] cl_bp_731:w-[300px] cl_bp_520:top-[4px] cl_bp_295:w-[200px] cl_bp_520:w-[265px] cl_bp_985:w-[300px]"
-      } cl_bp_985:ml-[50px] z-51 cursor-pointer ease-linear duration-[.3s] 
+	return (
+		<div
+			ref={logoTextRef}
+			className={`grid grid-cols-1 gap-[10px] w-[171px] ml-[15px] mb-[8px] justify-self-start self-center cl_bp_295:ml-[20px]  ${
+				props.logoScrolled === "true"
+					? "fixed top-[14px] cl_bp_731:w-[260px] cl_bp_520:top-[10px] cl_bp_295:w-[160px] cl_bp_520:w-[225px] cl_bp_985:w-[260px]"
+					: "absolute top-[4px] cl_bp_731:w-[300px] cl_bp_520:top-[4px] cl_bp_295:w-[200px] cl_bp_520:w-[265px] cl_bp_985:w-[300px]"
+			} cl_bp_985:ml-[50px] z-30 cursor-pointer ease-linear duration-[.3s] 
       }`}
-    >
-      <Image
-        className="justify-self-start w-[95%] -ml-[2px] cl_bp_295:ml-[initial]"
-        src={company_logo}
-        alt="floridablaze home"
-        width={300}
-      />
-      <Image
-        className={`justify-self-start w-[112%] ml-[1px] ease-linear duration-[.3s]   ${
-          props.logoScrolled === "true"
-            ? "opacity-0 max-h-[0px] max-w-[0px] scale(.1)"
-            : "opacity-100 max-h-[50px] max-w-[500px] scale(1)"
-        }`}
-        src={tag_line}
-        alt=""
-        width={300}
-      />
-    </div>
-  );
+		>
+			<Image
+				className="justify-self-start w-[95%] -ml-[2px] cl_bp_295:ml-[initial]"
+				src={company_logo}
+				alt="floridablaze home"
+				width={300}
+			/>
+			<Image
+				className={`justify-self-start w-[112%] ml-[1px] ease-linear duration-[.3s]   ${
+					props.logoScrolled === "true"
+						? "opacity-0 max-h-[0px] max-w-[0px] scale(.1)"
+						: "opacity-100 max-h-[50px] max-w-[500px] scale(1)"
+				}`}
+				src={tag_line}
+				alt=""
+				width={300}
+			/>
+		</div>
+	)
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,26 @@
-"use client";
-import Image from "next/image";
-import React, { useEffect, useState, useRef } from "react";
+"use client"
+import Image from "next/image"
+import React, { useEffect, useState, useRef } from "react"
 
-import leaf from "@/images/headerLeafv2.png";
-import CompanyLogo from "@/components/companyLogo";
+import leaf from "@/images/headerLeafv2.png"
+import CompanyLogo from "@/components/companyLogo"
 export default function Header() {
-  const [logoScrolled, setLogoScrolled] = React.useState("false");
+	const [logoScrolled, setLogoScrolled] = React.useState("false")
 
-  return (
-    <div className="relative grid grid-cols-[minmax(180px,280px)1fr] grid-rows-[69px] gap-[8px] mt-[15px] max-w-[2000px] overflow-hidden cl_bp_520:grid-rows-[80px]">
-      <div className="w-full" />
-      <CompanyLogo
-        logoScrolled={logoScrolled}
-        setLogoScrolled={setLogoScrolled}
-      />
-      <Image
-        className="hidden leaf_bp_520px:block w-full h-full max-w-[420px] min-w-[250px] justify-self-end self-center"
-        src={leaf}
-        alt=""
-      />
-    </div>
-  );
+	return (
+		<div className="relative grid grid-cols-[minmax(180px,280px)1fr] grid-rows-[69px] gap-[8px] mt-[15px] max-w-[2000px] overflow-hidden cl_bp_520:grid-rows-[80px]">
+			<div className="w-full" />
+			<div
+				className={`fixed ${
+					logoScrolled === "true" ? "opacity-100" : "opacity-0"
+				} top-0 ease-linear duration-[.3s] z-20 w-full max-w[2000px] min-w=[269px] h-[50px] bg-[#f4f4f4]`}
+			/>
+			<CompanyLogo logoScrolled={logoScrolled} setLogoScrolled={setLogoScrolled} />
+			<Image
+				className="hidden z-30 leaf_bp_520px:block w-full h-full max-w-[420px] min-w-[250px] justify-self-end self-center"
+				src={leaf}
+				alt=""
+			/>
+		</div>
+	)
 }
