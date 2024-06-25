9c299d9a534c1a25c712b317d39ceb0d420da9ce
Alejandro Raffo <amsterdamAL@gmail.com>
2024-06-25 09:00:34 -0400

    hamburger menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-"use client"
-import Image from "next/image"
-import Header from "@/components/header"
-import React, { useEffect, useState, useRef } from "react"
+"use client";
+import Image from "next/image";
+import Header from "@/components/header";
+import React, { useEffect, useState, useRef } from "react";
 
 export default function Home() {
-	const [openSideMenu, setOpenSideMenu] = useState("false")
+  const [openSideMenu, setOpenSideMenu] = useState("false");
 
-	return (
-		<div>
-			<Header openSideMenu={openSideMenu} setOpenSideMenu={setOpenSideMenu} />
-			<h1 className="h-[70vh]">news</h1>
-			<h1 className="h-[70vh]">act</h1>
-			<h1 className="h-[70vh]">join</h1>
-		</div>
-	)
+  return (
+    <div>
+      <Header openSideMenu={openSideMenu} setOpenSideMenu={setOpenSideMenu} />
+      <h1 className="h-[70vh]">news</h1>
+      <h1 className="h-[70vh]">act</h1>
+      <h1 className="h-[70vh]">join</h1>
+    </div>
+  );
 }
--- src/components/hamburger.tsx
+++ src/components/hamburger.tsx
@@ -1,94 +1,98 @@
-"use client"
+"use client";
 //import Image from "next/image"
-import React, { useEffect, useState, useRef } from "react"
+import React, { useEffect, useState, useRef } from "react";
 interface Props {
-	openSideMenu: string
-	setOpenSideMenu: (word: string) => void
+  openSideMenu: string;
+  setOpenSideMenu: (word: string) => void;
 }
 export default function Hamburger(props: Props) {
-	const [hamburgerScrolled, setHamburgerScrolled] = React.useState("false")
-	const [pixlesFromHamburgerToTop, setPixlesFromHamburgerToTop] = useState(1)
-	const hamburgerRef = useRef<HTMLInputElement>(null)
-	let listener: any
+  const [hamburgerScrolled, setHamburgerScrolled] = React.useState("false");
+  const [pixlesFromHamburgerToTop, setPixlesFromHamburgerToTop] = useState(1);
+  const hamburgerRef = useRef<HTMLInputElement>(null);
+  let listener: any;
 
-	useEffect(() => {
-		if (hamburgerRef.current) {
-			hamburgerRef.current.getBoundingClientRect().top > 37
-				? null
-				: setPixlesFromHamburgerToTop(hamburgerRef.current.getBoundingClientRect().top - 20)
-		}
+  useEffect(() => {
+    if (hamburgerRef.current) {
+      hamburgerRef.current.getBoundingClientRect().top > 37
+        ? null
+        : setPixlesFromHamburgerToTop(
+            hamburgerRef.current.getBoundingClientRect().top - 20
+          );
+    }
 
-		window.addEventListener("scroll", handleScroll)
-		return () => window.removeEventListener("scroll", handleScroll)
-	}, [])
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-	useEffect(() => {
-		console.log("UseEffect 5555555555555")
-		// make it so this header doesnt load on certin pages
+  useEffect(() => {
+    console.log("UseEffect 5555555555555");
+    // make it so this header doesnt load on certin pages
 
-		//function to pass to mousedown listener, to close side menu if need be.
-		listener = (event: any) => {
-			if (hamburgerRef.current) {
-				//if you click in the menu,  dont close it
-				if (hamburgerRef.current.contains(event.target)) {
-					return
-				}
-			}
+    //function to pass to mousedown listener, to close side menu if need be.
+    listener = (event: any) => {
+      if (hamburgerRef.current) {
+        //if you click in the menu,  dont close it
+        if (hamburgerRef.current.contains(event.target)) {
+          return;
+        }
+      }
 
-			//if you click anywhere outside the side menu, close it.
-			//mouseDownHandler();
-			props.setOpenSideMenu("false")
-		}
+      //if you click anywhere outside the side menu, close it.
+      //mouseDownHandler();
+      props.setOpenSideMenu("false");
+    };
 
-		//resize and/or orientationchange listener
-		const handleResize = () => {
-			//closed sideMenu on orientation change, if it gets bigger than 850px
-			if (window.innerWidth > 500) {
-				props.setOpenSideMenu("false")
-			}
-		}
+    //resize and/or orientationchange listener
+    const handleResize = () => {
+      //closed sideMenu on orientation change, if it gets bigger than 850px
+      if (window.innerWidth > 500) {
+        props.setOpenSideMenu("false");
+      }
+    };
 
-		//set up event listeners
-		window.addEventListener("resize", handleResize)
-		window.addEventListener("orientationchange", handleResize)
-		document.addEventListener("mousedown", listener)
+    //set up event listeners
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("orientationchange", handleResize);
+    document.addEventListener("mousedown", listener);
 
-		return () => {
-			window.removeEventListener("resize", handleResize)
-			window.removeEventListener("orientationchange", handleResize)
-			document.removeEventListener("mousedown", listener)
-			console.log("cleanup")
-		}
-	}, [hamburgerRef])
-	//}, [hamburgerRef, mouseDownHandler]);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("orientationchange", handleResize);
+      document.removeEventListener("mousedown", listener);
+      console.log("cleanup");
+    };
+  }, [hamburgerRef]);
+  //}, [hamburgerRef, mouseDownHandler]);
 
-	// sets the state so the element can stick
-	const handleScroll = () => {
-		window.scrollY >= pixlesFromHamburgerToTop + 10
-			? setHamburgerScrolled("true")
-			: setHamburgerScrolled("false")
-	}
+  // sets the state so the element can stick
+  const handleScroll = () => {
+    window.scrollY >= pixlesFromHamburgerToTop + 10
+      ? setHamburgerScrolled("true")
+      : setHamburgerScrolled("false");
+  };
 
-	return (
-		<div
-			className={`${
-				hamburgerScrolled === "true"
-					? "mt-0 fixed pr-0 top-[17px]"
-					: "mt-[8px] grid pr-[22px] top-[initial]"
-			}`}
-			ref={hamburgerRef}
-		>
-			<button
-				onClick={() => {
-					props.setOpenSideMenu(props.openSideMenu == "true" ? "false" : "true")
-				}}
-			>
-				<div />
-				<div />
-				<div />
-			</button>
-		</div>
-	)
+  return (
+    <div
+      className={`${
+        hamburgerScrolled === "true"
+          ? "mt-0 fixed pr-0 top-[17px]"
+          : "mt-[8px] grid pr-[22px] top-[initial]"
+      }`}
+      ref={hamburgerRef}
+    >
+      <button
+        onClick={() => {
+          props.setOpenSideMenu(
+            props.openSideMenu == "true" ? "false" : "true"
+          );
+        }}
+      >
+        <div />
+        <div />
+        <div />
+      </button>
+    </div>
+  );
 }
 
 // open_side_menu={props.open_side_menu}
@@ -98,16 +102,16 @@ export default function Hamburger(props: Props) {
 // scroll_dir={props.scroll_dir}
 
 // margin-top: ${(props) => (props.hamburger_scrolled == "true" ? "0px" : "8px")};
+// position: ${(props) => (props.hamburger_scrolled == "true" ? "fixed" : "grid")};
+// padding-right: ${(props) => (props.hamburger_scrolled == "true" ? "0px" : "22px")};
+// top: ${(props) => (props.hamburger_scrolled == "true" ? "17px" : "initial")};
+
 // grid-area: 1/2/2/3;
 // justify-self: end;
 // align-self: center;
-// position: ${(props) => (props.hamburger_scrolled == "true" ? "fixed" : "grid")};
-// padding-right: ${(props) => (props.hamburger_scrolled == "true" ? "0px" : "22px")};
 // //top: 25px;
 // right: 22px;
 // //transition: all 0.2s linear;
-
-// top: ${(props) => (props.hamburger_scrolled == "true" ? "17px" : "initial")};
 
 // @media only screen and (max-width: 520px) {
 //   align-self: start;
