ed60b9aed41ab283a455e1bd47ad4012ca3e775c
Alejandro Raffo <amsterdamAL@gmail.com>
2024-08-15 19:07:46 -0400

    jwt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-import "server-only";
+import "server-only"
 //import Image from "next/image"
 //import React, { useEffect, useState, useRef } from "react";
-import HomeClientWrapper from "../components/homeClientWrapper";
-import Home from "@/components/home";
-import Header from "@/components/header";
-import SignUp from "../components/signUp";
+import HomeClientWrapper from "../components/homeClientWrapper"
+import Home from "@/components/home"
+import Header from "@/components/header"
+import SignUp from "../components/signUp"
 //import "../css/global";
 
 export default function Page() {
-  //const [openSideMenu, setOpenSideMenu] = useState("false");
+	//const [openSideMenu, setOpenSideMenu] = useState("false");
 
-  return (
-    <>
-      <Header />
+	return (
+		<>
+			<Header />
 
-      <HomeClientWrapper>
-        <Home data="123" />
-      </HomeClientWrapper>
+			<HomeClientWrapper>
+				{/* @ts-expect-error Server Component */}
+				<Home data="123" />
+			</HomeClientWrapper>
 
-      {/* @ts-expect-error Server Component */}
-      <SignUp />
-    </>
-  );
+			<SignUp />
+		</>
+	)
 }
